c4da8a67cd293814dca86db11d36e00ebd46fc2f
ZLoganZ <ln26805@gmail.com>
2022-11-25 07:25:35 +0000

    data

--- src/main/java/context/DBContext.java
+++ src/main/java/context/DBContext.java
@@ -34,7 +34,7 @@ public class DBContext {
     private final String serverName = "localhost";
     private final String dbName = "QuanLyBanGiay";
     //private final String portNumber = "1433";
-    private final String instance="LOGANZ";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String instance="SQLEXPRESS";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
     private final String userID = "sa";
-    private final String password = "yendaika";
+    private final String password = "Webserver123";
 }
