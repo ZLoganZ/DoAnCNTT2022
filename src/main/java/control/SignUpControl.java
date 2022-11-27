9220ee21ad6dd60c7390e79b2ea64aac7904f031
Nguyễn Hoàng Hải <ln26805@gmail.com>
2022-11-27 21:06:46 +0700

    update code

--- src/main/java/control/SignUpControl.java
+++ src/main/java/control/SignUpControl.java
@@ -11,9 +11,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet(name = "SignUpControl", urlPatterns = {"/signup"})
@@ -32,21 +34,27 @@ public class SignUpControl extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String user = request.getParameter("user");
+        String name = request.getParameter("name");
         String pass = request.getParameter("pass");
         String re_pass = request.getParameter("repass");
         String email = request.getParameter("email");
         if(!pass.equals(re_pass)){
-            response.sendRedirect("Login.jsp");
+        	request.setAttribute("error", "Password does not match!");
+        	request.getRequestDispatcher("Signup.jsp").forward(request, response);
+            //response.sendRedirect("Login.jsp");
         }else{
             DAO dao = new DAO();
             Account a = dao.checkAccountExist(user);
             if(a == null){
                 //dc signup
-                dao.singup(user, pass, email);
-                response.sendRedirect("login");
+                dao.singup(user, name, pass, email);
+                request.setAttribute("mess", "Sign Up Success!");
+                request.getRequestDispatcher("Signup.jsp").forward(request, response);
+                //response.sendRedirect("login");
             }else{
-                //day ve trang login.jsp
-                response.sendRedirect("Login.jsp");
+            	request.setAttribute("error", "Account already exists!");
+            	request.getRequestDispatcher("Signup.jsp").forward(request, response);
+                //response.sendRedirect("Login.jsp");
             }
         }
         //sign up
@@ -64,7 +72,8 @@ public class SignUpControl extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        request.getRequestDispatcher("Signup.jsp").forward(request, response);
     }
 
     /**
